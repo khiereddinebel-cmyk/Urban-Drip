10c293da9ddbbeebe76fd3c157d7366ab7de743a
khiereddinebel-cmyk <khiereddinebel@gmail.com>
2026-06-07 18:10:35 +0100

    Correct Google Map iframe embed coordinates to 36.6474282, 2.6919874

--- src/presentation/pages/Home.tsx
+++ src/presentation/pages/Home.tsx
@@ -260,7 +260,7 @@ export default function Home({ featured = [] }: HomeProps) {
                     className="block w-full max-w-4xl mx-auto h-[250px] md:h-[350px] relative pointer-events-auto overflow-hidden rounded-sm shadow-md border border-gray-200"
                 >
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3198.71884399222!2d2.6935296!3d36.651008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128f9d0062a803e3%3A0x982db2fd36775a0f!2sUrban%20drip!5e0!3m2!1sen!2s!4v1700000000000!5m2!1sen!2s"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3198.71884399222!2d2.6919874!3d36.6474282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128f9d0062a803e3%3A0x982db2fd36775a0f!2sUrban%20drip!5e0!3m2!1sen!2s!4v1700000000000!5m2!1sen!2s"
                         width="100%"
                         height="100%"
                         style={{ border: 0 }}
